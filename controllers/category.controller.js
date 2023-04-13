0a4b8924eef34fb6fe0d491c1cd5a40ab313dc6a
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-04-13 21:13:40 +0600

    Added category routes

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,4 +1,4 @@
-const { createCategoryService } = require("../services/category.service")
+const { createCategoryService, allCategoryService, deleteCategoryService, updateCategoryService } = require("../services/category.service")
 
 exports.createCategory = async (req, res) => {
     try {
@@ -12,6 +12,59 @@ exports.createCategory = async (req, res) => {
             message: 'Category Created',
             data: category
         })
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            error: error.message
+        })
+    }
+}
+
+exports.allCategory = async (req, res) => {
+    try {
+        const { categories, total } = await allCategoryService()
+
+        res.status(200).json({
+            status: "success",
+            message: "All Categories",
+            total,
+            data: total > 0 ? categories : "No Categories Found",
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            error: error.message
+        })
+    }
+}
+
+exports.deleteCategory = async (req, res) => {
+    try {
+        await deleteCategoryService(req.params.categoryId)
+
+        res.status(200).json({
+            status: "success",
+            message: "Deleted successfully"
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            error: error.message
+        })
+    }
+}
+
+exports.updateCategory = async (req, res) => {
+    try {
+        await updateCategoryService(req.params.categoryId, req.body)
+
+        res.status(200).json({
+            status: "success",
+            message: "Updated successfully"
+        })
+
     } catch (error) {
         res.status(500).json({
             status: "fail",
--- routes/category.route.js
+++ routes/category.route.js
@@ -4,12 +4,12 @@ const router = express.Router()
 const verifyToken = require('../middlewares/verifyToken')
 const categoryController = require('../controllers/category.controller')
 
-router.get('/all', verifyToken, categoryController.createCategory)
+router.get('/all', verifyToken, categoryController.allCategory)
 
 router.post('/create', verifyAdmin, categoryController.createCategory)
 
-// router.route('/:invId')
-// .get(verifyToken, invController.findInvById)
-// .delete(verifyAdmin, invController.deleteInvoice)
+router.route('/:categoryId')
+.delete(verifyAdmin, categoryController.deleteCategory)
+.post(verifyAdmin, categoryController.updateCategory)
 
 module.exports = router
